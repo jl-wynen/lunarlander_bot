10715d1a322bb74daa7e1eeb5eb0fd64f188b580
Neil Vaytet <neil.vaytet@ess.eu>
2024-01-15 21:50:06 +0100

    update

--- bot.py
+++ bot.py
@@ -4,8 +4,36 @@ import numpy as np
 
 from lunarlander import Instructions
 
-
 CREATOR = "Apollo 11"  # This is your team name
+
+
+def rotate(current, target):
+    if np.abs(current - target) < 0.35:
+        return
+    return "left" if current < target else "right"
+
+
+def find_landing_site(terrain):
+    # Find largest landing site
+    n = len(terrain)
+    # Find run starts
+    loc_run_start = np.empty(n, dtype=bool)
+    loc_run_start[0] = True
+    np.not_equal(terrain[:-1], terrain[1:], out=loc_run_start[1:])
+    run_starts = np.nonzero(loc_run_start)[0]
+
+    # Find run lengths
+    run_lengths = np.diff(np.append(run_starts, n))
+
+    # Find largest run
+    imax = np.argmax(run_lengths)
+    start = run_starts[imax]
+    end = start + run_lengths[imax]
+
+    if (end - start) > 40:
+        loc = int(start + (end - start) * 0.5)
+        print("Found landing site at", loc)
+        return loc
 
 
 class Bot:
@@ -23,13 +51,9 @@ class Bot:
         self,
         t: float,
         dt: float,
-        x: float,
-        y: float,
-        heading: float,
-        vx: float,
-        vy: float,
-        fuel: float,
         terrain: np.ndarray,
+        players: dict,
+        asteroids: list,
     ):
         """
         This is the method that will be called at every time step to get the
@@ -38,27 +62,39 @@ class Bot:
         Parameters
         ----------
         t:
-            The current time in hours.
+            The current time in seconds.
         dt:
-            The time step in hours.
+            The time step in seconds.
+        terrain:
+            The (1d) array representing the lunar surface altitude.
+        players:
+            A dictionary of the players in the game. The keys are the team names and
+            the values are the information about the players.
+        asteroids:
+            A list of the asteroids currently flying.
         """
         instructions = Instructions()
+
+        me = players[self.team]
+        x, y = me["position"]
+        vx, vy = me["velocity"]
 
         if self.initial_manoeuvre:
             if vx > 10:
                 instructions.main = True
             elif self.target_heading is not None:
-                command = self.rotate(heading)
+                command = rotate(current=me["heading"], target=0)
                 if command == "left":
                     instructions.left = True
                 elif command == "right":
                     instructions.right = True
-            if self.target_heading is None:
-                self.initial_manoeuvre = False
+                else:
+                    # if self.target_heading is None:
+                    self.initial_manoeuvre = False
             return instructions
 
         if self.target_site is None:
-            self.target_site = self.find_landing_site(terrain)
+            self.target_site = find_landing_site(terrain)
 
         if (self.target_site is None) and (y < 900) and (vy < 0):
             instructions.main = True
@@ -69,11 +105,11 @@ class Bot:
             if np.abs(diff) < 50:
                 if vx > 0.1:
                     self.target_heading = 90
-                    command = self.rotate(heading)
+                    command = self.rotate(me["heading"])
                     instructions.main = True
                 else:
                     self.target_heading = 0
-                    command = self.rotate(heading)
+                    command = self.rotate(me["heading"])
                     instructions.main = False
 
                 if command == "left":
@@ -87,38 +123,4 @@ class Bot:
                 if vy < 0:
                     instructions.main = True
 
-        if t < 5:
-            instructions.main = True
-
         return instructions
-
-    def rotate(self, heading):
-        if np.abs(heading - self.target_heading) < 0.35:
-            self.target_heading = None
-            return
-        if heading < self.target_heading:
-            return "left"
-        else:
-            return "right"
-
-    def find_landing_site(self, terrain):
-        # Find largest landing site
-        n = len(terrain)
-        # Find run starts
-        loc_run_start = np.empty(n, dtype=bool)
-        loc_run_start[0] = True
-        np.not_equal(terrain[:-1], terrain[1:], out=loc_run_start[1:])
-        run_starts = np.nonzero(loc_run_start)[0]
-
-        # Find run lengths
-        run_lengths = np.diff(np.append(run_starts, n))
-
-        # Find largest run
-        imax = np.argmax(run_lengths)
-        start = run_starts[imax]
-        end = start + run_lengths[imax]
-
-        if (end - start) > 40:
-            loc = int(start + (end - start) * 0.5)
-            print("Found landing site at", loc)
-            return loc
