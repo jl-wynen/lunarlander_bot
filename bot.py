8dd69e769d41abf0f6938a3fda6dd030831ab47b
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-02-03 19:08:27 +0100

    More thrust

--- bot.py
+++ bot.py
@@ -230,5 +230,7 @@ class Bot:
         would_stop_at = stop_at_if_slow_down(h0=y, v0=vy, heading=heading)
         if would_stop_at < target_height - 40:
             instructions.main = True
+        if abs(y - terrain[self.target_site]) < 10:
+            instructions.main = True
 
         return instructions, self.land
